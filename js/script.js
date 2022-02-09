11a1ddcb167764669471ea69ec36b22fc0fedb66
Khabibullo Abduvahidov <khabibullo@vakhid.digital>
2022-02-09 16:15:43 +0500

    website created

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Frontend Developer","Streamer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
